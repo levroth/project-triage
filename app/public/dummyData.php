5abc39223d2c72ec0a4f0aaff5d9e2d74856ebe5
Levi Roth <levroth@iu.edu>
2019-09-09 13:21:10 -0400

    Vue work, dummy data made, styles updated

---
+++ app/public/dummyData.php
@@ -0,0 +1,23 @@
+[
+  {
+    "patientGuid" : "50p",
+    "firstName" : "Bob",
+    "lastName" : "Jones",
+    "dob" : "1996-11-22",
+    "sexAtBirth" : "M"
+  },
+  {
+    "patientGuid" : "51p",
+    "firstName" : "Susan",
+    "lastName" : "Jones",
+    "dob" : "1990-12-20",
+    "sexAtBirth" : "F"
+  },
+  {
+    "patientGuid" : "52p",
+    "firstName" : "Bob",
+    "lastName" : "Smith",
+    "dob" : "1950-12-12",
+    "sexAtBirth" : "M"
+  }
+];
