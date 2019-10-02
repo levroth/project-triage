16237e016934cbcc7203e0d4d335d1d67e579ffc
Levi Roth <levroth@iu.edu>
2019-10-02 09:43:57 -0400

    Class updates to post to database

---
+++ app/public/api/waiting/post.php
@@ -0,0 +1,23 @@
+<?php
+
+// 0. Validate my data
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  'INSERT INTO PatientVisit
+    (patientGuid, visitDescription, priority)
+  VALUES (?,?,?)'
+);
+$stmt->execute([
+  $_POST['patientGuid'],
+  $_POST['visitDescription'],
+  $_POST['priority']
+]);
+
+// TODO: error checking?!
+
+header('HTTP/1.1 303 GET it elsewhere');
+header('Location: ../waiting/');
