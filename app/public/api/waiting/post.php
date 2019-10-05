b9506aac82d95942b5e2d8f67e1fa1a573da1146
Levi Roth <levroth@iu.edu>
2019-10-05 19:06:54 -0400

    Attempt at fixing code

--- app/public/api/waiting/post.php
+++ app/public/api/waiting/post.php
@@ -1,23 +1,20 @@
+
 <?php
-
 // 0. Validate my data
-
 // Step 1: Get a datase connection from our help class
 $db = DbConnection::getConnection();
-
 // Step 2: Create & run the query
 $stmt = $db->prepare(
-  'INSERT INTO PatientVisit
-    (patientGuid, visitDescription, priority)
-  VALUES (?,?,?)'
+'INSERT INTO PatientVisit
+  (patientGuid, visitDescription, priority)
+VALUES (?,?,?)'
 );
 $stmt->execute([
-  $_POST['patientGuid'],
-  $_POST['visitDescription'],
-  $_POST['priority']
+$_POST['patientGuid'],
+$_POST['visitDescription'],
+$_POST['priority']
 ]);
-
-// TODO: error checking?!
-
-header('HTTP/1.1 303 GET it elsewhere');
+// TODO: Error checking?!
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
 header('Location: ../waiting/');
--- app/public/js/triage.app.js
+++ app/public/js/triage.app.js
@@ -5,21 +5,21 @@ var patientTriageApp = new Vue({
   },
   methods: {
     handleSubmit() {
-        fetch('api/waiting/post.php', {
-            method:'POST',
-            body: JSON.stringify(this.patient),
-            headers: {
-              "Content-Type": "applciation/json; charset=utf-8"
-            }
-        })
-        .then( response => response.json() )
-        .then( json => {waitingApp.patients = json})
-        .catch( err => {
-          console.error('TRIAGE POST ERROR');
-          console.error(err);
-        });
+       fetch('api/waiting/post.php', {
+         method:'POST',
+         body: JSON.stringify(this.patient),
+         headers: {
+           "Content-Type": "application/json; charset=utf-8"
+         }
+       })
+       .then( response => response.json() )
+       .then( json => {waitingApp.patients = json})
+       .catch( err => {
+         console.error('TRIAGE POST ERROR:');
+         console.error(err);
+      });
 
-       //waitingApp.patients.push(this.patient);
+       // waitingApp.patients.push(this.patient);
        this.handleReset();
     },
     handleReset() {
